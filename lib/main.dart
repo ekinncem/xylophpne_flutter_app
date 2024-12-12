634339ea63c52397ca4bffa217772f9b630a9c42
ekinncem <ekinn.ciem16@gmail.com>
2024-12-12 21:36:01 +0300

    new

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(AssetSource('sounds/note1.wav'));
+                  },
+                  child: const Text('Note 1'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(AssetSource('sounds/note2.wav'));
+                  },
+                  child: const Text('Note 2'),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.play(AssetSource('sounds/note3.wav'));
+                  },
+                  child: const Text('Note 3'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
